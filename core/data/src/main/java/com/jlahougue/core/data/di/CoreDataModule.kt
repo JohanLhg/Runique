a83a6cc17ad94b6b67ae50eaa3c0cf3d4093e8a4
JohanLhg <johan.lhg@outlook.fr>
2024-05-25 12:36:46 +0200

    Authentication Tokens (fixed)

--- core/data/src/main/java/com/jlahougue/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/jlahougue/core/data/di/CoreDataModule.kt
@@ -9,7 +9,7 @@ import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
-        HttpClientFactory().build()
+        HttpClientFactory(get()).build()
     }
     singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
